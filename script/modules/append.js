eadd995dbe43f09af2e7a0cc6bd5f3396d6dc766
Rick Groot <rickgroot41@gmail.com>
2021-03-03 12:14:37 +0100

    updated routing

--- script/modules/append.js
+++ script/modules/append.js
@@ -4,6 +4,9 @@ import {
 import {
     details
 } from "../detailpage.js";
+import {
+    data
+} from "./data.js";
 
 const cont = document.getElementById('list');
 
@@ -14,17 +17,15 @@ if (cont.children.length === 0) {
 
 // appends an image
 function appendPosts(post) {
+    let a = document.createElement('a')
     let article = document.createElement('article');
     let img = document.createElement('img');
     img.src = post.url; //sets src of image
+    a.href = '#post/' + post.id;
     article.id = "image1";
     article.appendChild(img);
-    article.onclick = () => {
-        router('post/' + post.id); // defines route
-        location.href = "/#post/" + post.id; // changes your url
-        details(post); // renders details page
-    }
-    cont.appendChild(article);
+    a.appendChild(article);
+    cont.appendChild(a);
 
     //removes placeholder when container has children
     if (cont.children.length > 0) {
